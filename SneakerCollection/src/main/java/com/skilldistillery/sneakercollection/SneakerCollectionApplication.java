51e6cdf395297e6148b91e1ad01043f3173d14e6
kristentsuboi <kristenatsuboi@gmail.com>
2023-05-12 19:10:44 -0400

    linked jpa to spring

---
+++ SneakerCollection/src/main/java/com/skilldistillery/sneakercollection/SneakerCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sneakercollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerCollectionApplication.class, args);
+	}
+
+}
---
+++ SneakerCollection/src/test/java/com/skilldistillery/sneakercollection/SneakerCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sneakercollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
